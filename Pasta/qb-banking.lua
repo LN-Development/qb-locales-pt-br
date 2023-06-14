c9a11b93ff53014039bffb38b488100ffc36436e
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-14 20:38:59 -0300

    Update qb-banking.lua

--- Pasta/qb-banking.lua
+++ Pasta/qb-banking.lua
@@ -1,35 +1,35 @@
 local Translations = {
     error = {
-        not_give = "Could not give item to the given id.",
-        givecash = "Usage /givecash [ID] [AMOUNT]",
-        wrong_id = "Wrong ID.",
-        dead = "You are dead LOL.",
-        too_far_away = "You are too far away lmfao.",
-        not_enough = "You don\'t have this amount.",
-        invalid_amount = "Invalid Amount Given"
+        not_give = "Não foi possível fornecer o item ao ID fornecido.",
+        givecash = "Use /givecash [ID] [QUANTIDADE]",
+        wrong_id = "ID incorreto.",
+        dead = "Você está morto.",
+        too_far_away = "Você está muito longe.",
+        not_enough = "Você não possui esta quantidade.",
+        invalid_amount = "Quantidade inválida fornecida."
     },
     success = {
-        debit_card = "You have successfully ordered a Debit Card.",
-        cash_deposit = "You successfully made a cash deposit of $%{value}.",
-        cash_withdrawal = "You successfully made a cash withdrawal of $%{value}.",
-        updated_pin = "You have successfully updated your debit card pin.",
-        savings_deposit = "You successfully made a savings deposit of $%{value}.",
-        savings_withdrawal = "You successfully made a savings withdrawal of $%{value}.",
-        opened_savings = "You have successfully opened a savings account.",
-        give_cash = "Successfully gave $%{cash} to ID %{id}",
-        received_cash = "Successfully received $%{cash} from ID %{id}"
+        debit_card = "Você solicitou com sucesso um Cartão de Débito"
+        cash_deposit = "Você fez um depósito em dinheiro de $%{value} com sucesso.",
+        cash_withdrawal = "Você fez um saque em dinheiro de $%{value} com sucesso.",
+        updated_pin = "Você atualizou com sucesso o PIN do seu cartão de débito.",
+        savings_deposit = "Você fez um depósito na conta poupança de $%{value} com sucesso",
+        savings_withdrawal = "Você fez um saque na conta poupança de $%{value} com sucesso.",
+        opened_savings = "Você abriu uma conta poupança com sucesso.",
+        give_cash = "Enviou com sucesso $%{cash} para o ID %{id}",
+        received_cash = "Recebeu com sucesso $%{cash} do ID %{id}"
     },
     info = {
-        bank_blip = "Bank",
-        access_bank_target = "Access Bank",
-        access_bank_key = "[E] - Access Bank",
-        current_to_savings = "Transfer Current Account to Savings",
-        savings_to_current = "Transfer Savings to Current Account",
-        deposit = "Deposit $%{amount} into Current Account",
-        withdraw = "Withdraw $%{amount} from Current Account",
+        bank_blip = "Banco",
+        access_bank_target = "Acessar Banco",
+        access_bank_key = "[E] - Acessar Banco",
+        current_to_savings = "Transferir da Conta Corrente para a Poupança",
+        savings_to_current = "Transferir da Poupança para a Conta Corrente",
+        deposit = "Depositar $%{amount} na Conta Corrente"",
+        withdraw = "Sacar $%{amount} da Conta Corrente",
     },
     command = {
-        givecash = "Give cash to player."
+        givecash = "Dar dinheiro a um jogador."
     }
 }
 
