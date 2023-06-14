a03bc892c27e68054f056e286bc7f167b4094b2b
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-14 20:56:46 -0300

    Update qb-busjob.lua

--- Pasta/qb-busjob.lua
+++ Pasta/qb-busjob.lua
@@ -1,25 +1,25 @@
 local Translations = {
     error = {
-        already_driving_bus = 'You are already driving a bus',
-        not_in_bus = 'You are not in a bus',
-        one_bus_active = 'You can only have one active bus at a time',
-        drop_off_passengers = 'Drop off the passengers before you stop working'
+        already_driving_bus = 'Você já está dirigindo um ônibus',
+        not_in_bus = 'Você não está dentro de um ônibus',
+        one_bus_active = 'Você só pode ter um ônibus ativo de cada vez',
+        drop_off_passengers = 'Deixe os passageiros saírem antes de parar de trabalhar'
     },
     success = {
-        dropped_off = 'Person was dropped off',
+        dropped_off = 'A pessoa foi deixada',
     },
     info = {
-        bus = 'Standard Bus',
-        goto_busstop = 'Go to the bus stop',
-        busstop_text = '[E] Bus Stop',
+        bus = 'Ônibus Padrão',
+        goto_busstop = 'Ir para o ponto de ônibus',
+        busstop_text = '[E] Ponto de Ônibus',
         bus_plate = 'BUS', -- Can be 3 or 4 characters long (uses random 4 digits)
-        bus_depot = 'Bus Depot',
-        bus_stop_work = '[E] Stop Working',
-        bus_job_vehicles = '[E] Job Vehicles'
+        bus_depot = 'Garagem de Ônibus',
+        bus_stop_work = '[E] Parar de Trabalhos',
+        bus_job_vehicles = '[E] Veículos de Trabalho'
     },
     menu = {
-        bus_header = 'Bus Vehicles',
-        bus_close = '⬅ Close Menu'
+        bus_header = 'Ônibus',
+        bus_close = '⬅ Fechar'
     }
 }
 
