ae73a8faa5c1b5ff4dd6e23c73786082ebd369ea
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-16 18:30:11 -0300

    Update qb-jewelery.lua

--- Traduções/qb-jewelery.lua
+++ Traduções/qb-jewelery.lua
@@ -1,18 +1,18 @@
 local Translations = {
     error = {
-        fingerprints = 'You\'ve left a fingerprint on the glass',
-        minimum_police = 'Minimum of %{value} police needed',
-        wrong_weapon = 'Your weapon is not strong enough..',
-        to_much = 'You have to much in your pocket'
+        fingerprints = 'Você deixou uma impressão digital no vidro',
+        minimum_police = 'É necessário um mínimo de %{value} policiais',
+        wrong_weapon = 'Sua arma não é forte o suficiente...',
+        to_much = 'Você está carregando coisas demais no bolso'
     },
     success = {},
     info = {
-        progressbar = 'Smashing the display case',
+        progressbar = 'Quebrando a vitrine',
     },
     general = {
-        target_label = 'Smash the display case',
-        drawtextui_grab = '[E] Smash the display case',
-        drawtextui_broken = 'Display case is broken'
+        target_label = 'Quebrar a vitrine',
+        drawtextui_grab = '[E] Quebrar a vitrine',
+        drawtextui_broken = 'Vitrine quebrada'
     }
 }
 
