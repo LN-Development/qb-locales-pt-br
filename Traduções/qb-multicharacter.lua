12edb9b08d6dc02d32492058c7d98f6e78ffc3f2
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-16 18:48:36 -0300

    Update qb-multicharacter.lua

--- Traduções/qb-multicharacter.lua
+++ Traduções/qb-multicharacter.lua
@@ -1,73 +1,73 @@
 local Translations = {
     notifications = {
-        ["char_deleted"] = "Character deleted!",
-        ["deleted_other_char"] = "You successfully deleted the character with citizen id %{citizenid}.",
-        ["forgot_citizenid"] = "You forgot to input a citizen id!",
+        ["char_deleted"] = "Personagem excluído!",
+        ["deleted_other_char"] = "Você excluiu com sucesso o personagem com ID do cidadão %{citizenid}.",
+        ["forgot_citizenid"] = "Você esqueceu de inserir um ID do cidadão!",
     },
 
     commands = {
         -- /deletechar
-        ["deletechar_description"] = "Deletes another players character",
-        ["citizenid"] = "Citizen ID",
-        ["citizenid_help"] = "The Citizen ID of the character you want to delete",
+        ["deletechar_description"] = "Exclui o personagem de outro jogador",
+        ["citizenid"] = "ID do Cidadão",
+        ["citizenid_help"] = "O ID do cidadão do personagem que você deseja excluir",
 
         -- /logout
-        ["logout_description"] = "Logout of Character (Admin Only)",
+        ["logout_description"] = "Fazer logout do personagem (Apenas Admin)",
 
         -- /closeNUI
-        ["closeNUI_description"] = "Close Multi NUI"
+        ["closeNUI_description"] = "Fechar Multi NUI"
     },
 
     misc = {
-        ["droppedplayer"] = "You have disconnected from QBCore"
+        ["droppedplayer"] = "Você desconectou de JL Network"
     },
 
     ui = {
         -- Main
-        characters_header = "My Characters",
-        emptyslot = "Empty Slot",
-        play_button = "Play",
-        create_button = "Create Character",
-        delete_button = "Delete Character",
+        characters_header = "Meus Personagens",
+        emptyslot = "Slot Vazio",
+        play_button = "Jogar",
+        create_button = "Criar Personagem",
+        delete_button = "Excluir Personagem",
 
         -- Character Information
-        charinfo_header = "Character Information",
-        charinfo_description = "Select a character slot to see all information about your character.",
-        name = "Name",
-        male = "Male",
-        female = "Female",
-        firstname = "First Name",
-        lastname = "Last Name",
-        nationality = "Nationality",
-        gender = "Gender",
-        birthdate = "Birthdate",
-        job = "Job",
-        jobgrade = "Job Grade",
-        cash = "Cash",
-        bank = "Bank",
-        phonenumber = "Phone Number",
-        accountnumber = "Account Number",
+        charinfo_header = "Informações do Personagem",
+        charinfo_description = "Selecione um slot de personagem para ver todas as informações sobre seu personagem.",
+        name = "Nome",
+        male = "Homem",
+        female = "Mulher",
+        firstname = "Nome",
+        lastname = "Sobrenome",
+        nationality = "Nacionalidade",
+        gender = "Gênero",
+        birthdate = "Data de Nascimento",
+        job = "Trabalho",
+        jobgrade = "Nível do Emprego",
+        cash = "Dinheiro",
+        bank = "Banco",
+        phonenumber = "Número de Telefone",
+        accountnumber = "Número da Conta",
 
-        chardel_header = "Character Registration",
+        chardel_header = "Registro de Personagem",
 
         -- Delete character
-        deletechar_header = "Delete Character",
-        deletechar_description = "Are You Sure You Want To Delete Your Character?",
+        deletechar_header = "Deletar Personagem",
+        deletechar_description = "Tem certeza de que deseja excluir seu personagem?",
 
         -- Buttons
-        cancel = "Cancel",
-        confirm = "Confirm",
+        cancel = "Cancelar",
+        confirm = "Confirmar",
 
         -- Loading Text
-        retrieving_playerdata = "Retrieving player data",
-        validating_playerdata = "Validating player data",
-        retrieving_characters = "Retrieving characters",
-        validating_characters = "Validating characters",
+        retrieving_playerdata = "Obtendo dados do jogador",
+        validating_playerdata = "Validando dados do jogador",
+        retrieving_characters = "Obtendo personagens",
+        validating_characters = "Validando personagens",
 
         -- Notifications
-        ran_into_issue = "We ran into an issue",
-        profanity = "It seems like you are trying to use some type of profanity / bad words in your name or nationality!",
-        forgotten_field = "It seems like you have forgotten to input one or multiple of the fields!"
+        ran_into_issue = "Enfrentamos um problema",
+        profanity = "Parece que você está tentando usar palavras ofensivas ou impróprias no seu nome ou nacionalidade!",
+        forgotten_field = "Parece que você esqueceu de preencher um ou vários campos!"
     }
 }
 
