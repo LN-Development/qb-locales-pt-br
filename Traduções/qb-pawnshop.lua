5b59a2fb6be6cd66be2ff4ddd6f7dba9ed089ead
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-16 18:59:06 -0300

    Update qb-pawnshop.lua

--- Traduções/qb-pawnshop.lua
+++ Traduções/qb-pawnshop.lua
@@ -1,31 +1,31 @@
 local Translations = {
     error = {
-        negative = 'Trying to sell a negative amount?',
-        no_melt = 'You didn\'t give me anything to melt...',
-        no_items = 'Not enough items',
-        inventory_full = 'Inventory too full to receive all possible items. Try making sure inventory isn\'t full next time. Items Lost: %{value}'
+        negative = 'Tentando vender uma quantidade negativa?',
+        no_melt = 'Você não me deu nada para derreter...',
+        no_items = 'Itens insuficientes',
+        inventory_full = 'Inventário muito cheio para receber todos os itens possíveis. Tente garantir que o inventário não esteja cheio da próxima vez. Itens Perdidos: %{value}'
     },
     success = {
-        sold = 'You have sold %{value} x %{value2} for $%{value3}',
-        items_received = 'You received %{value} x %{value2}',
+        sold = 'Você vendeu %{value} x %{value2} por $%{value3}',
+        items_received = 'Você recebeu %{value} x %{value2}',
     },
     info = {
         title = 'Pawn Shop',
-        subject = 'Melting Items',
-        message = 'We finished melting your items. You can come pick them up at any time.',
-        open_pawn = 'Open the Pawn Shop',
-        sell = 'Sell Items',
-        sell_pawn = 'Sell Items To The Pawn Shop',
-        melt = 'Melt Items',
-        melt_pawn = 'Open the Melting Shop',
-        melt_pickup = 'Pickup Melted Items',
-        pawn_closed = 'Pawnshop is closed. Come back between %{value}:00 AM - %{value2}:00 PM',
-        sell_items = 'Selling Price $%{value}',
-        back = '⬅ Go Back',
-        melt_item = 'Melt %{value}',
-        max = 'Max Amount %{value}',
-        submit = 'Melt',
-        melt_wait = 'Give me %{value} minutes and I\'ll have your stuff melted',
+        subject = 'Derretimento de Itens',
+        message = 'Nós terminamos de derreter seus itens. Você pode vir buscá-los a qualquer momento.',
+        open_pawn = 'Abrir a Pawn Shop',
+        sell = 'Vender Itens',
+        sell_pawn = 'Vender Itens para a Pawn Shop',
+        melt = 'Vender Itens',
+        melt_pawn = 'Abrir a Loja de Derretimento',
+        melt_pickup = 'Retirar Itens Derretidos',
+        pawn_closed = 'Pawnshop está fechada. Volte entre %{value}:00 AM - %{value2}:00 PM',
+        sell_items = 'Preço de Venda $%{value}',
+        back = '⬅ Voltar',
+        melt_item = 'Derreter  %{value}',
+        max = 'Quantidade Máxima %{value}',
+        submit = 'Derreter',
+        melt_wait = 'Dê-me %{value} minutos e vou derreter suas coisas',
     }
 }
 
