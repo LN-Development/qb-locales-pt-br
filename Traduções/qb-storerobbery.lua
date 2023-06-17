af9101de31f68632c587114e4524b1df87803ff7
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-17 12:19:28 -0300

    Update qb-storerobbery.lua

--- Traduções/qb-storerobbery.lua
+++ Traduções/qb-storerobbery.lua
@@ -1,22 +1,22 @@
 local Translations = {
     error = {
-        minimum_store_robbery_police = "Not Enough Police (%{MinimumStoreRobberyPolice} Required)",
-        not_driver = "You Are Not The Driver",
-        demolish_vehicle = "You Are Not Allowed To Demolish Vehicles Now",
-        process_canceled = "Process canceled..",
-        you_broke_the_lock_pick = "You Broke The Lock Pick",
+        minimum_store_robbery_police = "Não há polícia suficiente (%{MinimumStoreRobberyPolice} necessário)",
+        not_driver = "Você não é o motorista",
+        demolish_vehicle = "Você não tem permissão para demolir veículos agora",
+        process_canceled = "Processo cancelado..",
+        you_broke_the_lock_pick = "Você quebrou a chave de abrir",
     },
     text = {
-        the_cash_register_is_empty = "The Cash Register Is Empty",
-        try_combination = "~g~E~w~ - Try Combination",
-        safe_opened = "Safe Opened",
-        emptying_the_register= "Emptying The Register..",
-        safe_code = "Safe Code: "
+        the_cash_register_is_empty = "O caixa está vazio",
+        try_combination = "~g~E~w~ - Tentar combinação",
+        safe_opened = "Cofre aberto",
+        emptying_the_register= "Esvaziando o caixa..",
+        safe_code = "Código do cofre: "
     },
     email = {
-        shop_robbery = "10-31 | Shop Robbery",
-        someone_is_trying_to_rob_a_store = "Someone Is Trying To Rob A Store At %{street} (CAMERA ID: %{cameraId1})",
-        storerobbery_progress = "Storerobbery in progress"
+        shop_robbery = "QRU | Roubo de Loja",
+        someone_is_trying_to_rob_a_store = "Alguém está tentando roubar uma loja em %{street}",
+        storerobbery_progress = "Roubo em andamento"
     },
 }
 
