705b86f576bb637fb3c55cd6fb537857eeed0d84
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-17 13:03:07 -0300

    Update qb-tunerchip.lua

--- Traduções/qb-tunerchip.lua
+++ Traduções/qb-tunerchip.lua
@@ -1,20 +1,20 @@
 local Translations = {
     success = {
-        this_vehicle_has_been_tuned = "This Vehicle Has Been Tuned",
+        this_vehicle_has_been_tuned = "Este veículo foi ajustado",
     },
     text = {
-        this_is_not_the_idea_is_it = "This is not the idea, is it?",
-        connecting_nos = "Connecting NOS...",
+        this_is_not_the_idea_is_it = "Esta não é a ideia, certo?",
+        connecting_nos = "Conectando NOS...",
     },
     error = {
-        tunerchip_vehicle_tuned = "TunerChip v1.05: Vehicle Tuned!",
-        this_vehicle_has_not_been_tuned = "This Vehicle Has Not Been Tuned",
-        no_vehicle_nearby = "No Vehicle Nearby",
-        tunerchip_vehicle_has_been_reset = "TunerChip v1.05: Vehicle has been reset!",
-        you_are_not_in_a_vehicle = "You Are Not In A Vehicle",
-        you_cannot_do_that_from_this_seat = "You cannot do that from this seat!",
-        you_already_have_nos_active = "You Already Have NOS Active",
-        canceled = "Canceled",
+        tunerchip_vehicle_tuned = "TunerChip v1.05: Veículo ajustado!",
+        this_vehicle_has_not_been_tuned = "Este veículo não foi ajustado",
+        no_vehicle_nearby = "Nenhum veículo próximo",
+        tunerchip_vehicle_has_been_reset = "TunerChip v1.05: O veículo foi redefinido!",
+        you_are_not_in_a_vehicle = "Você não está em um veículo",
+        you_cannot_do_that_from_this_seat = "Você não pode fazer isso deste assento!",
+        you_already_have_nos_active = "Você já tem NOS ativado",
+        canceled = "Cancelado",
     },
 }
 
