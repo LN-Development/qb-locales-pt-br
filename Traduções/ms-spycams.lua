d17508092ace9ac14a8842f0fddae95d0812a715
Andradeeeeeee <136652404+Andradeeeeeee@users.noreply.github.com>
2023-06-18 14:29:50 -0300

    Update ms-spycams.lua

--- Traduções/ms-spycams.lua
+++ Traduções/ms-spycams.lua
@@ -1,29 +1,29 @@
 local Translations = {
     controls = {
-        cancel      = "Cancel",
-        place       = "Place Spycam",
-        disconnect  = "Disconnect",
-        destroy     = "Self-destruct",
-        mode        = "Vision Mode",
-        zoomin      = "Zoom In",
-        zoomout     = "Zoom Out",
-        moveright   = "Move Right",
-        movedown    = "Move Down",
-        moveleft    = "Move Left",
-        moveup      = "Move Up",        
+        cancel      = "Cancelar",
+        place       = "Colocar Espião",
+        disconnect  = "Desconectar",
+        destroy     = "Auto-destruir",
+        mode        = "Modo de Visão",
+        zoomin      = "Aproximar",
+        zoomout     = "Afastar",
+        moveright   = "Mover para a Direita",
+        movedown    = "Mover para Baixo",
+        moveleft    = "Mover para a Esquerda",
+        moveup      = "Mover para Cima",        
     },
     
     errors = {
-        placed        = "You have the maximum number of spy cameras deployed",
-        missing       = "You don't have any spy cameras deployed",
-        invalid       = "Invalid placement",
-        range         = "Camera out of range - cannot send self-destruct signal",
+        placed        = "Você já possui o número máximo de câmeras espiãs implantadas",
+        missing       = "Você não possui nenhuma câmera espiã implantada",
+        invalid       = "Colocação inválida",
+        range         = "Câmera fora de alcance - não é possível enviar sinal de auto-destruição",
     },
 
     general = {
-        destroy     = "Spy camera will self-destruct in %{time} seconds",
-        destroyed   = "Spy camera destroyed",
-        nosignal    = "NO SIGNAL",
+        destroy     = "A câmera espiã se auto-destruirá em %{time} segundos",
+        destroyed   = "Câmera espiã destruída",
+        nosignal    = "SEM SINAL",
     }
 }
 
