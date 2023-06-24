c84cd606a3dbf94b165f45aa693b0be4941df991
Luan <123782375+LN-Development@users.noreply.github.com>
2023-06-24 13:42:32 -0300

    Create m-drugs.lua

---
+++ Traduções/m-drugs.lua
@@ -0,0 +1,167 @@
+Language = {
+    ["General"] = {
+        ["Notifications"] = {
+            ["JobRequired"] = "You can't do that.",
+            ["Failed"] = "Failed.",
+            ["SemNecessario"] = "You dont have what i need",
+            ["SemTesoura"] = "You don't have any scissors.",
+            ["SemTesouras"] = "You don't have enough scissors.",
+            ["SemSaco"] = "You don't have bags.",
+            ["SemSacos"] = "You don't have enough bags.",
+        }
+    },
+    ["Weed"] = {
+        ["Notifications"] = {
+            ["PickWeedSucess"] = "Leaf harvested successfully.",
+            ["CanceledWeed"] = "Canceled.",
+            ["SemWeed"] = "You don't have weed with you.",
+            ["SemWeeds"] = "You don't have enough weed.",
+        },
+        ["Targets"] = {
+            ["CutPlant"] = "Cut Plant",
+            ["IconCutPlant"] = "fas fa-car",
+            ["ProcessWeed"] = "Talk",
+            ["IconProcessWeed"] = "fas fa-male",
+            ["PackageWeed"] = "Talk",
+            ["IconPackageWeed"] = "fas fa-male",
+        },
+        ["ProgressBars"] = {
+            ["PickingWeed"] = "Harvesting weed...",
+            ["ProcessingWeed"] = "Processing weed...",
+            ["PackageWeed"] = "Packing weed...",
+        },
+        ["QBMenu"] = {
+            -- Process
+            ["ProcessWeedTitle"] = "Process Weed",
+            ["ProcessWeedLowQuallity"] = "Process Low QL Weed",
+            ["RequiredWeedLowQuallity"] = "Required: <br> 1x Low QL Weed",
+            ["ProcessWeedMedQuallity"] = "Process Medium QL Weed",
+            ["RequiredWeedMedQuallity"] = "Required: <br> 1x Medium QL Weed",
+            ["ProcessWeedMaxQuallity"] = "Process High QL Weed",
+            ["RequiredWeedMaxQuallity"] = "Required: <br> 1x High QL Weed",
+            -- Packaged
+            ["PackageWeedTitle"] = "Pack Weed",
+            ["PackageWeedLowQuallity"] = "Packing Low QL Weed Leaves",
+            ["PackageWeedLowQuallity2"] = "Required: <br> 1x Low QL Weed Leaves",
+            ["PackageWeedMedQuallity"] = "Pack Folhas de Erva Média QL",
+            ["PackageWeedMedQuallity2"] = "Required: <br> 1x Medium QL Weed Leaves",
+            ["PackageWeedMaxQuallity"] = "Pack Folhas de Erva Alta QL",
+            ["PackageWeedMaxQuallity2"] = "Required: <br> 1x High QL Weed Leaves",
+        }
+    },
+    ["Coke"] = {
+        ["Notifications"] = {
+            ["PickCokeSucess"] = "Successfully harvested cocaine leaf.",
+            ["CanceledCoke"] = "Canceled.",
+            ["SemCoke"] = "You don't have cocaine with you.",
+            ["SemCokes"] = "Not enough cocaine.",
+        },
+        ["Targets"] = {
+            ["CutPlant"] = "Cut Plant",
+            ["IconCutPlant"] = "fas fa-seedling",
+            ["ProcessCoke"] = "Talk",
+            ["IconProcessCoke"] = "fas fa-male",
+            ["PackageCoke"] = "Talk",
+            ["IconPackageCoke"] = "fas fa-male",
+        },
+        ["ProgressBars"] = {
+            ["PickingCoke"] = "Taking cocaine...",
+            ["ProcessingCoke"] = "Processing cocaine...",
+            ["PackageCoke"] = "Packing cocaine...",
+        },
+        ["QBMenu"] = {
+            ["ProcessCokeTitle"] = "Process Cocaine",
+            ["ProcessCokeLowQuallity"] = "Process Low Quality Cocaine",
+            ["RequiredCokeLowQuallity"] = "Required: <br> 1x Low Quality Cocaine",
+            ["ProcessCokeMedQuallity"] = "Processar Cocaína Média Qualidade",
+            ["RequiredCokeMedQuallity"] = "Required: <br> 1x Medium Quality Cocaine",
+            ["ProcessCokeMaxQuallity"] = "Processar Cocaína Alta Qualidade",
+            ["RequiredCokeMaxQuallity"] = "Required: <br> 1x High Quality Cocaine",
+            -- Packaged
+            ["PackageCokeTitle"] = "Pack Cocaine",
+            ["PackageCokeLowQuallity"] = "Packaging Low Quality Cocaine",
+            ["PackageCokeLowQuallity2"] = "Required: <br> 1x Low Quality Cocaine Leaves",
+            ["PackageCokeMedQuallity"] = "Pack Medium Quality Cocaine",
+            ["PackageCokeMedQuallity2"] = "Required: <br> 1x Medium Quality Cocaine Leaves",
+            ["PackageCokeMaxQuallity"] = "Pack High Quality Cocaine",
+            ["PackageCokeMaxQuallity2"] = "Required: <br> 1x High Quality Cocaine Leaves",
+
+        }
+    },
+    ["Meth"] = {
+        ["Notifications"] = {
+            ["PickMethSucess"] = "Successfully harvested methamphetamine leaf.",
+            ["CanceledMeth"] = "Canceled.",
+            ["SemMeth"] = "You don't have meth on you.",
+            ["SemMeths"] = "Not enough methamphetamines.",
+        },
+        ["Targets"] = {
+            ["CutPlant"] = "Cut Plant",
+            ["IconCutPlant"] = "fas fa-seedling",
+            ["ProcessMeth"] = "Talk",
+            ["IconProcessMeth"] = "fas fa-male",
+            ["PackageMeth"] = "Talk",
+            ["IconPackageMeth"] = "fas fa-male",
+        },
+        ["ProgressBars"] = {
+            ["PickingMeth"] = "Taking methamphetamines...",
+            ["ProcessingMeth"] = "Processing methamphetamine...",
+            ["PackageMeth"] = "Packing the methamphetamines...",
+        },
+        ["QBMenu"] = {
+            ["ProcessMethTitle"] = "Process Methamphetamines",
+            ["ProcessMethLowQuallity"] = "Process Low Quality Methamphetamines",
+            ["RequiredMethLowQuallity"] = "Required: <br> 1x Low Quality Methamphetamines",
+            ["ProcessMethMedQuallity"] = "Processar Methamphetamines Medium Quality",
+            ["RequiredMethMedQuallity"] = "Required: <br> 1x Medium Quality Methamphetamines",
+            ["ProcessMethMaxQuallity"] = "Processar Methamphetamines Alta Qualidade",
+            ["RequiredMethMaxQuallity"] = "Required: <br> 1x Medium High Methamphetamines",
+            -- Packaged
+            ["PackageMethTitle"] = "Pack Methamphetamine",
+            ["PackageMethLowQuallity"] = "Packaging Low Quality Methamphetamine",
+            ["PackageMethLowQuallity2"] = "Required: <br> 1x Low Quality Methamphetamine Leaves",
+            ["PackageMethMedQuallity"] = "Pack Methamphetamine Medium Quality",
+            ["PackageMethMedQuallity2"] = "Required: <br> 1x Medium Quality Methamphetamine Leaves",
+            ["PackageMethMaxQuallity"] = "Pack High Quality Methamphetamine",
+            ["PackageMethMaxQuallity2"] = "Required: <br> 1x High Quality Methamphetamine Leaves",
+        }
+    },
+    ["Opium"] = {
+        ["Notifications"] = {
+            ["PickOpiumSucess"] = "Successfully harvested opium leaf.",
+            ["CanceledOpium"] = "Canceled.",
+            ["SemOpium"] = "You don't have opium with you.",
+            ["SemOpiums"] = "Not enough opium.",
+        },
+        ["Targets"] = {
+            ["CutPlant"] = "Cut Plant",
+            ["IconCutPlant"] = "fas fa-car",
+            ["ProcessOpium"] = "Talk",
+            ["IconProcessOpium"] = "fas fa-male",
+            ["PackageOpium"] = "Talk",
+            ["IconPackageOpium"] = "fas fa-male",
+        },
+        ["ProgressBars"] = {
+            ["PickingOpium"] = "Picking up opium...",
+            ["ProcessingOpium"] = "Processing the opium...",
+            ["PackageOpium"] = "Packing the opium...",
+        },
+        ["QBMenu"] = {
+            ["ProcessOpiumTitle"] = "Process Opium",
+            ["ProcessOpiumLowQuallity"] = "Process Opium Low Quality",
+            ["RequiredOpiumLowQuallity"] = "Required: <br> 1x Low Quality Opium",
+            ["ProcessOpiumMedQuallity"] = "Process Opium Medium Quality",
+            ["RequiredOpiumMedQuallity"] = "Required: <br> 1x Medium Quality Opium",
+            ["ProcessOpiumMaxQuallity"] = "Process Opium High Quality",
+            ["RequiredOpiumMaxQuallity"] = "Required: <br> 1x High Quality Opium",
+            -- Packaged
+            ["PackageOpiumTitle"] = "Pack Opium",
+            ["PackageOpiumLowQuallity"] = "Packaging Low Quality Opium",
+            ["PackageOpiumLowQuallity2"] = "Required: <br> 1x Low Quality Opium Leaves",
+            ["PackageOpiumMedQuallity"] = "Packaging Medium Quality Opium",
+            ["PackageOpiumMedQuallity2"] = "Required: <br> 1x Medium Quality Opium Leaves",
+            ["PackageOpiumMaxQuallity"] = "Pack High Quality Opium",
+            ["PackageOpiumMaxQuallity2"] = "Required: <br> 1x High Quality Opium Leaves",
+        }
+    },
+}
